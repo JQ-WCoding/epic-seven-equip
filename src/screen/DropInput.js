2b469f435be6905bd54a63c1069c98c297871b24
Jun_Q <junlee36520620@gmail.com>
2023-01-09 23:06:23 +0900

    변환 수치 계산 적용

--- src/screen/DropInput.js
+++ src/screen/DropInput.js
@@ -3,14 +3,50 @@ import React, {useState} from "react";
 import {options} from "../options/options";
 
 const DropInput = ( props ) => {
+
     const [dropDownOptions] = useState( options.map( ( {label} ) => {
         return label
     } ) );
 
-    const onChanged = ( args ) => {
-        const filteredData = options.filter( ( {label} ) => label === args.value );
+    const [maxNumber, setMaxNumber] = useState( "0" );
+    const [minNumber, setMinNumber] = useState( "0" );
+    const [result, setResult] = useState( 0 );
+    const [value, setValue] = useState( "0" );
+    const [weightedValue, setWeightedValue] = useState(1);
 
-        console.log( 'filteredData', filteredData );
+    const onChanged = ( args ) => {
+        const filteredData = options.filter( ( {label} ) => label === args.value )[0];
+
+        switch ( filteredData.division ) {
+            case 'percentage':
+                console.log( 'percentage' );
+
+                setRangeValue( filteredData.minValue, filteredData.maxValue );
+                setWeightedValue( filteredData.weightedValue );
+                break;
+            case 'pure':
+                console.log( 'pure' );
+
+                setRangeValue( 1, 5 );
+                setWeightedValue( filteredData.weightedValue );
+                break;
+            default:
+                console.log( 'nothig' );
+
+                setRangeValue( 0, 0 );
+                break;
+        }
+    }
+
+    const inputOnChanged = ( args ) => {
+        setValue( args.target.value );
+        setResult( args.target.value * weightedValue );
+    }
+
+    const setRangeValue = ( min, max ) => {
+        setMinNumber( min.toString() );
+        setMaxNumber( max.toString() );
+        setValue( min.toString() );
     }
 
     return (
@@ -18,9 +54,12 @@ const DropInput = ( props ) => {
             <Dropdown options={dropDownOptions}
                       onChange={onChanged} key={props.dropDownKey}/>
 
-            <input key={props.inputKey} type="number" defaultValue="0"/>
+            <input key={props.inputKey} type="number" value={value} min={minNumber} max={maxNumber}
+                   onChange={inputOnChanged}/>
+
+            <div>변환 수치 : {result}</div>
         </>
-    )
+    );
 }
 
 export default DropInput;
--- src/screen/Main.js
+++ src/screen/Main.js
@@ -3,23 +3,36 @@ import DropInput from "./DropInput";
 
 const Main = () => {
 
-    const inputItems = [
-        {dropDownKey: 1, inputKey: 1},
-        {dropDownKey: 2, inputKey: 2},
-        {dropDownKey: 3, inputKey: 3},
-        {dropDownKey: 4, inputKey: 4},
-        {dropDownKey: 5, inputKey: 5}
-    ];
+    // const inputItems = [
+    //     {dropDownKey: 1, inputKey: 1},
+    //     {dropDownKey: 2, inputKey: 2},
+    //     {dropDownKey: 3, inputKey: 3},
+    //     {dropDownKey: 4, inputKey: 4},
+    //     {dropDownKey: 5, inputKey: 5}
+    // ];
 
-    const dropInputList = inputItems.map( ( item ) => <DropInput dropDownKey={item.dropDownKey}
-                                                                 inputKey={item.inputKey}/> )
+    // const dropInputList = inputItems.map( ( item ) => <DropInput dropDownKey={item.dropDownKey}
+    //                                                              inputKey={item.inputKey}/> )
+
+    const onClick = () => {
+
+    }
 
     return (
         <>
             <div>
-                {array.map( ( each ) => <DropInput dropDownKey={each} inputKey={each}/> )}
+                {/*{array.map( ( each ) => <DropInput dropDownKey={each} inputKey={each}/> )}*/}
                 <DropInput dropDownKey={1} inputKey={11}/>
-                {/*{dropInputList}*/}
+                <DropInput dropDownKey={2} inputKey={12}/>
+                <DropInput dropDownKey={3} inputKey={13}/>
+                <DropInput dropDownKey={4} inputKey={14}/>
+            </div>
+
+
+            <div>
+                <p>장비 점수</p>
+                <button onClick={onClick}>계산하기</button>
+                <input type="number" readOnly={true}/>
             </div>
         </>
     )
